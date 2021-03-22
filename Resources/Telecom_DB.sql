c94e86c03186fa979697174164092867ef374105
Wes90 <70103986+Wes90@users.noreply.github.com>
2021-03-22 17:38:08 -0600

    sql commit

---
+++ Resources/Telecom_DB.sql
@@ -0,0 +1,24 @@
+CREATE TABLE Telecom (
+	CustomerID VARCHAR,
+	Gender VARCHAR,
+	SeniorCitizen CHAR,
+	Partner VARCHAR,
+	Dependents VARCHAR,
+	Tenure INT,
+	PhoneService VARCHAR,
+	MultipleLines VARCHAR,
+	InternetService VARCHAR,
+	OnlineSecurity VARCHAR,
+	OnlineBackup VARCHAR,
+	DeviceProtection VARCHAR,
+	TechSupport VARCHAR, 
+	StreamingTV VARCHAR,
+	StreamingMovies VARCHAR,
+	Contract VARCHAR,
+	PaperlessBilling VARCHAR,
+	PaymentMethod VARCHAR,
+	MonthlyCharges INT,
+	TotalCharges INT,
+	Churn VARCHAR
+);
+
