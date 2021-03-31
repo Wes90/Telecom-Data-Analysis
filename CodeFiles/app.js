c4cec6add6351eec619917a4b1c18d925a961a47
Wes90 <70103986+Wes90@users.noreply.github.com>
2021-03-30 23:35:37 -0600

    html commit

--- CodeFiles/app.js
+++ CodeFiles/app.js
@@ -1,7 +1,32 @@
-var viz
-function initViz() {
-  var containerDiv = document.getElementById("vizContainer"),
-  url = "https://us-east-1.online.tableau.com/t/zuar/views/Regional/GlobalTemperatures";
+// var viz
+// function initViz() {
+//   var Dashboard = document.getElementById("vizContainer"),
+//   url = "https://public.tableau.com/views/Book1_16166499785510/Dashboard1?:language=en&:display_count=y&publish=yes&:origin=viz_share_link";
 
-  viz = new tableau.Viz(containerDiv, url);
+//   viz = new tableau.Viz(Dashboard, url);
+
+// var viz2
+
+//   var PieCharts = document.getElementById("vizContainer"),
+//   url2 = "https://public.tableau.com/views/Book1_16166499785510/Sheet5?:language=en&:display_count=y&publish=yes&:origin=viz_share_link";
+
+//   viz2 = new tableau.Viz(PieCharts, url2);
+// }
+function initViz() {
+  url = "https://public.tableau.com/views/Book1_16166499785510/Dashboard1?:language=en&:display_count=y&publish=yes&:origin=viz_share_link",
+  options = {
+      hideToolbar: true,
+      width: "1300px",
+      height: "1050px",
+  };
+  viz = new tableau.Viz(Dashboard, url, options);
+
+  url2= "https://public.tableau.com/views/Book1_16166499785510/Dashboard2?:language=en&:display_count=y&publish=yes&:origin=viz_share_link",
+  options = {
+      hideToolbar: true,
+      width: "1300px",
+      height: "1050px",
+  };
+  viz2 = new tableau.Viz(PieCharts, url2, options);
 }
+  
