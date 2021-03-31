74406056c1805e7cde43a2721ab70938cc19d323
Wes90 <70103986+Wes90@users.noreply.github.com>
2021-03-31 01:29:00 -0600

    ml image

--- CodeFiles/app.js
+++ CodeFiles/app.js
@@ -1,17 +1,3 @@
-// var viz
-// function initViz() {
-//   var Dashboard = document.getElementById("vizContainer"),
-//   url = "https://public.tableau.com/views/Book1_16166499785510/Dashboard1?:language=en&:display_count=y&publish=yes&:origin=viz_share_link";
-
-//   viz = new tableau.Viz(Dashboard, url);
-
-// var viz2
-
-//   var PieCharts = document.getElementById("vizContainer"),
-//   url2 = "https://public.tableau.com/views/Book1_16166499785510/Sheet5?:language=en&:display_count=y&publish=yes&:origin=viz_share_link";
-
-//   viz2 = new tableau.Viz(PieCharts, url2);
-// }
 function initViz() {
   url = "https://public.tableau.com/views/Book1_16166499785510/Dashboard1?:language=en&:display_count=y&publish=yes&:origin=viz_share_link",
   options = {
