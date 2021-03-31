7ba224699c249776d76165d03119d1662dac5333
Wes90 <70103986+Wes90@users.noreply.github.com>
2021-03-31 16:47:20 -0600

    final commit

--- CodeFiles/app.js
+++ CodeFiles/app.js
@@ -14,5 +14,13 @@ function initViz() {
       height: "1050px",
   };
   viz2 = new tableau.Viz(PieCharts, url2, options);
+
+  url3= "https://public.tableau.com/views/Book1_16166499785510/Dashboard3?:language=en&:display_count=y&publish=yes&:origin=viz_share_link",
+  options = {
+      hideToolbar: true,
+      width: "1300px",
+      height: "1050px",
+  };
+  viz2 = new tableau.Viz(stackchart, url3, options);
 }
   
